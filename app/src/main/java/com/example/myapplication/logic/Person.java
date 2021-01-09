4729c4abf92e9c52d9cdb4bdae0604a81f26cc3f
Lilach26 <55951495+Lilach26@users.noreply.github.com>
2021-01-09 15:41:24 +0200

    login and register pages

---
+++ app/src/main/java/com/example/myapplication/logic/Person.java
@@ -0,0 +1,33 @@
+package com.example.myapplication.logic;
+
+public class Person
+{
+    private String email;
+    private String name;
+
+    public Person()
+    {
+    }
+
+    public Person(String name, String email)
+    {
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

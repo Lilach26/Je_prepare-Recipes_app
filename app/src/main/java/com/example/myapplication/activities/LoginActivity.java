d3c06c7423a0d11de9639bf8a6123ec2f9be19d1
Sapir Shemesh <39224974+shemeshsapir@users.noreply.github.com>
2021-01-09 17:36:09 +0200

    create user + authentication

--- app/src/main/java/com/example/myapplication/activities/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/activities/LoginActivity.java
@@ -1,16 +1,73 @@
 package com.example.myapplication.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText emailLogin;
+    private  EditText passwordLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void LoginFunc(View view) {
+        emailLogin = findViewById(R.id.emailLogin);
+        String email = emailLogin.getText().toString();
+
+        passwordLogin = findViewById(R.id.passwordLogin);
+        String password = passwordLogin.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, (task) -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(LoginActivity.this, "Login success.",
+                                Toast.LENGTH_LONG).show();
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                        Intent intent = new Intent (LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(LoginActivity.this, "Login failed.",
+                                Toast.LENGTH_LONG).show();
+
+                    }
+                });
+    }
+
+    public void MoveToRegister(View view) {
+        Intent intent = new Intent (LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myapplication/activities/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/activities/RegisterActivity.java
@@ -1,16 +1,80 @@
 package com.example.myapplication.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.logic.Person;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+
+    private EditText emailEditText;
+    private  EditText passwordEditText;
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void RegisterFunc(View view) {
+        emailEditText = findViewById(R.id.emailEditText);
+        String email = emailEditText.getText().toString();
+
+        passwordEditText = findViewById(R.id.passwordEditText);
+        String password = passwordEditText.getText().toString();
+
+        nameEditText = findViewById(R.id.nameEditText);
+        String name = nameEditText.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(RegisterActivity.this, "Register success.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String uid = user.getUid();
+
+                            Person person = new Person(name, email);
+                            HashMap<String, Person> map = new HashMap<>();
+                            map.put(uid, person);
+                            db.collection("Users").add(map);
+
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegisterActivity.this, "Register failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
     }
 }
